10d19446b8f3216eca6568982b5e2c69ca8724d7
Sean Sanders <seansanders@pop-os.localdomain>
2023-04-04 13:01:21 -0400

    Working on script to download apk files

--- Python/apkdownloader.py
+++ Python/apkdownloader.py
@@ -1,100 +1,37 @@
-import urllib3, os, re
+import urllib3, os, re, webbrowser
 from bs4 import BeautifulSoup
 from google_play_scraper import app, Sort, reviews_all, reviews, search
-from urllib.request import Request, urlopen
+from urllib.request import Request, urlopen, urlretrieve
 
-def open_app_in_browser(package_name):
-    url = f"https://play.google.com/store/apps/details?id={package_name}"
-    webbrowser.open(url)
+def get_download_links_from_apkpure():
+    # div.first.brand.is-brand.sa-all-div.sa-apps-div.mb
+    download_apk_links = []
+    for apk in list_of_apps:
+        link = None
+        print(' '.join([apk['appId'], "(",apk['title'], ")"]))
+        url = ''.join(['https://m.apkpure.com/',apk['title'].lower().replace(" ", "-").replace('®',''),'/',apk['appId'],'/download'])
+        print(url)
+        req = Request(
+        url=url, 
+        headers={'User-Agent': 'Mozilla/5.0'}
+        )
+        webpage = urlopen(req).read()
+        soup = BeautifulSoup(webpage, 'html.parser')
+        # print(soup)
+        link = soup.find('a', {'class', 'download-start-btn'})
+        if link is not None: 
+            # print("LINK TO DOWNLOAD APK:"+str(link['href']))
+            download_apk_links.append(str(link['href']))
+    return download_apk_links
 
-def get_top_10_apps(category):
-    # results = search('Games', num_results=10, sort=Sort.NEWEST)
-    results = search(
-    str(category),
-    lang="en",  # defaults to 'en'
-    country="us",  # defaults to 'us'
-    # sort=Sort.NEWEST,
-    n_hits=10  # defaults to 30 (= Google's maximum)
-)
-    # print(results)
-    return [result for result in results]
+def open_links_in_browser(download_apk_links):
+    for link in download_apk_links:
+        print(link)
+        last_item = link.split('/').pop().replace('?version=latest', '')
+        webbrowser.open(link)
 
-def get_apk_url(package_name): 
-    opener = urllib3.PoolManager()
-    headers = {
-        'authority': 'scrapeme.live',
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    }
-    response = opener.request('GET', 'http://apk-dl.com/' + package_name, headers=headers)
-    print(response.status)
-    # print(response.data)
-    soup = BeautifulSoup(response.data, 'html.parser')
-    temp_link = soup.find("div",{'class': 'download-btn'}).find("a")["href"]
+def move_file_from_downloads():
+    os.chdir('../APK')
+    print(os.listdir('/home/seansanders/Downloads/'))
 
-    response = opener.request('GET', 'http://apk-dl.com/' + temp_link, headers=headers)
-    soup = BeautifulSoup(response.data, 'html.parser')
-    temp_link2 = soup.find("section",{'class': 'detail'}).find("a")["href"]
-
-    print(temp_link2)
-    response = opener.request('GET', temp_link2, headers=headers)
-    soup = BeautifulSoup(response.data, 'html.parser')
-    print(soup)
-    temp_link3 = soup.find("div",{'class': 'contents'}).find("a")["href"]
-
-    return "http:" + temp_link3
-
-#print(get_apk_url('com.facebook.katana'))
-
-#print(get_apk_url("com.foxxymobile.singletapgames"))
-
-
-os.system('clear')
-opener = urllib3.PoolManager()
-headers = {
-    'authority': 'scrapeme.live',
-    'dnt': '1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-fetch-site': 'none',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'document',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-}
-
-list_of_apps = get_top_10_apps('games')
-# print(list_of_apps[0])
-# req = Request(
-# url='https://m.apkpure.com/search?q='+list_of_apps[0], 
-# headers={'User-Agent': 'Mozilla/5.0'}
-# )
-# webpage = urlopen(req).read()
-# soup = BeautifulSoup(webpage, 'html.parser')
-# div = soup.find("div", {"class", "first first-apk sa-all-div sa-apps-div mb"})
-# print(div.find('div', {"class", "button"}))
-
-
-
-# div.first.brand.is-brand.sa-all-div.sa-apps-div.mb
-for apk in list_of_apps:
-	print(' '.join([apk['appId'], "(",apk['title'], ")"]))
-	url = ''.join(['https://m.apkpure.com/',apk['title'].lower().replace(" ", "-").replace('®',''),'/',apk['appId'],'/download'])
-	print(url)
-	req = Request(
-	url=url, 
-	headers={'User-Agent': 'Mozilla/5.0'}
-	)
-	webpage = urlopen(req).read()
-	soup = BeautifulSoup(webpage, 'html.parser')
-	# print(soup)
-	link = soup.find('a', {'class', 'download-start-btn'})
-	print("LINK TO DOWNLOAD APK:"+str(link))
+move_file_from_downloads()
