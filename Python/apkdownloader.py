1f7176d7dab02d57f3c8bfaa3c45c0384bf4e025
Sean Sanders <seansanders@pop-os.localdomain>
2023-04-06 09:32:18 -0400

    Working on python code

--- Python/apkdownloader.py
+++ Python/apkdownloader.py
@@ -1,8 +1,8 @@
-import urllib3, os, re, webbrowser, shutil, time
+import urllib3, os, re, webbrowser, shutil, time, graphviz
 from bs4 import BeautifulSoup
 from google_play_scraper import app, Sort, reviews_all, reviews, search
 from urllib.request import Request, urlopen, urlretrieve
-
+from graphviz import Source
 def get_top_n_number_of_apps(category, num_apps_to_investigate):
     results = search(
     str(category),
@@ -85,7 +85,9 @@ def run_framework(APK_Name, Main_Class):
         # Soot_output_Location=../sootOutput/$1/
         # Main_Class=get_main_class_from_APK(APK_Name)
         os.chdir("Classes")
-        os.system(' '.join(['java -cp .:../../Jar_Libs/* SootAnalysis', APK_Location, Soot_output_Location, Main_Class]))
+        cmd = ' '.join(['java -cp .:../../Jar_Libs/* SootAnalysis', APK_Location, Soot_output_Location, Main_Class])
+        # print(cmd)
+        os.system(cmd)
         os.chdir('../')
     else:
         os.chdir("../Java")
@@ -94,7 +96,9 @@ def run_framework(APK_Name, Main_Class):
         # Soot_output_Location=../sootOutput/$1/
         # Main_Class=get_main_class_from_APK(APK_Name)
         os.chdir("Classes")
-        os.system(' '.join(['java -cp .:../../Jar_Libs/* SootAnalysis', APK_Location, Soot_output_Location, Main_Class]))
+        cmd = ' '.join(['java -cp .:../../Jar_Libs/* SootAnalysis', APK_Location, Soot_output_Location, Main_Class])
+        # print(cmd)
+        os.system(cmd)
         os.chdir('../')
 
 def get_main_class_from_APK(APK_Name):
@@ -113,6 +117,9 @@ def install_APK(APK_Name):
     if directory.__contains__("Python"):
         os.chdir("../Java/")
         os.system(''.join(['adb install sootOutput/',APK_Name,'/signed',APK_Name,'.apk']))
+    elif directory.__contains__("Java"):
+        # os.chdir("../Java/")
+        os.system(''.join(['adb install sootOutput/',APK_Name,'/signed',APK_Name,'.apk']))
 
 def uninstall_APK(package):
     os.system(' '.join(['adb uninstall',package]))
@@ -123,17 +130,82 @@ def log_APK(APK_Name):
     time.sleep(13)
     os.system('pkill -f adb')
     os.system('adb logcat -c')
+    # with open(''.join(['../ADB_Logcat_Logs/',APK_Name,'.txt'])) as f:
+    #     lines = f.readlines()
+
+    # for line in lines:
+    #     if not line.__contains__('beginning'):
+    #         print(line)
+
+def clear_screen():
+    os.system("clear")
+
+def zip_and_sign_APK_file(APK_Name):
+    if os.getcwd().__contains__('Java'):
+        os.system(''.join(['zipalign -fv 4 sootOutput/',APK_Name,'/',APK_Name,'.apk', ' sootOutput/',APK_Name,'/signed',APK_Name,'.apk']))
+        os.system(''.join(['apksigner sign --ks ../my-release-key.keystore --ks-pass pass:password sootOutput/',APK_Name,'/signed',APK_Name,'.apk']))
+
+def create_ngrams(lst, n):
+    return [tuple(lst[i:i + n]) for i in range(len(lst) - n + 1)]
+
+def add_edge(graph, src_node, dest_node, edge_label=None):
+    """
+    Adds an edge to the given Graphviz graph.
+
+    :param graph: The Graphviz graph object.
+    :param src_node: The source node identifier.
+    :param dest_node: The destination node identifier.
+    :param edge_label: The label for the edge (optional).
+    """
+    graph.edge(src_node, dest_node, label=edge_label)
+
+def generate_model_from_log(APK_Name):
+    # print(APK_Name)
+    
+    # dot.edge('App_Start', 'App_Start_No_Ad_Displayed')
+    
+
+    methods=[]
+    Ids=[]
     with open(''.join(['../ADB_Logcat_Logs/',APK_Name,'.txt'])) as f:
         lines = f.readlines()
 
     for line in lines:
         if not line.__contains__('beginning'):
-            print(line)
+            line_array = line.split('---')
+            methods.append(line_array[3])
+            Ids.append(line_array.pop().replace("\n",""))
 
-def clear_screen():
-    os.system("clear")
+    print(''.join(["Original list:",str(methods)]))
+    app_state_id_list=['App_Start', 'App_Start_No_Ad_Displayed','App_Start_adView_Set', 'App_Running_Ad_Registered', 
+    'App_Running_Ad_Loaded', 'App_Running_Ad_Displayed', 'App_Running_Ad_Impression', 'App_Start_Ad_Displayed']
+    app_state_list = ['The app has started', 'The app has started with no Ads displayed', 
+    'The app has started and an adView was set', 'The app is running and the advertisement is registe,red', 
+    'The app is running and the advertisement is loaded', 'The app is running and the advertisement is displayed', 
+    'The app is running and the advertisement impression is made', 'The app has started with Ads displayed']
 
+    list_states = ['']
+    dot = graphviz.Digraph('G',filename ="temp.gv")
 
+    # Add nodes using the provided state lists
+    for state_id, state_label in zip(app_state_id_list, app_state_list):
+        dot.node(state_id, label=state_label)
+    
+    for ngram in create_ngrams(methods, 2):
+        print(ngram)
+        # check_and_create_edge(dot,ngram)
+        if ngram[0].__contains__('onCreate') or (ngram[0].__contains__('onCreate') and ngram[1].__contains__('onCreate')):
+            print("opt1")
+            add_edge(dot, 'App_Start', 'App_Start', 'onCreate')
+        if ngram[0].__contains__('onCreate') and ngram[1].__contains__('findViewById'):
+            print("opt2")
+            add_edge(dot, 'App_Start', 'App_Start_No_Ad_Displayed', 'findViewById')
+        if ngram[0].__contains__('findViewById') and ngram[1].__contains__('findViewById'):
+            print("opt3")
+            add_edge(dot, 'App_Start_No_Ad_Displayed', 'App_Start_No_Ad_Displayed', 'findViewById')
+
+    print(dot.source)
+    dot.view()
 
 clear_screen()
 # app_categories_print()
@@ -151,11 +223,13 @@ clear_screen()
 #------------------------------------------------------------------------------------------------------------
 #-------------------------------------INSTRUMENT APK FILES---------------------------------------------------
 #------------------------------------------------------------------------------------------------------------
-# compile_framework()
+compile_framework()
 Main_Class = get_main_class_from_APK('TestApp3')
 APK_Package = get_package_from_APK('TestApp3')
-# run_framework('TestApp3', Main_Class)
-# install_APK('TestApp3')
-# clear_log()
+run_framework('TestApp3', Main_Class)
+zip_and_sign_APK_file('TestApp3')
+install_APK('TestApp3')
+
+# log_APK("Test")
 # uninstall_APK(APK_Package)
-log_APK("Test")
+# generate_model_from_log('Test')
