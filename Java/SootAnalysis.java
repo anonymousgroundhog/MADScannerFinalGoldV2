147760fa6f57ea97929709906c7fb7a49a15fc68
Sean Sanders <seansanders@pop-os.localdomain>
2023-04-24 09:41:46 -0400

    Working on hhashmap for tracking classes and methods

--- Java/SootAnalysis.java
+++ Java/SootAnalysis.java
@@ -29,36 +29,60 @@ public class SootAnalysis
         Scene thisScene = Scene.v();
         thisScene.loadNecessaryClasses();
         // PackManager.v().writeOutput();
-       
         // START INSTRUMENTING HERE
-        // Collection<SootClass> appClasses = thisScene.getApplicationClasses();
         Collection<SootClass> allClasses = thisScene.getClasses();
-        String adSpecificClass = "com.google.android.gms.ads";
-        for (SootClass cl : allClasses) {
-            for (SootMethod meth: cl.getMethods()){
-                if(meth.getName().contains("onCreate") && cl.getName().contains(sootarguments[2])){
-                    Print("Class:" + cl.getName());
-                    Print(meth.getName());
+        String advertising_specific_class = "com.google.android.gms.ads";
+        String super_class_of_interest = "com.google.android.gms.ads.BaseAdView";
+        Map<String, List<String>> dictionary_of_classes = new HashMap<>();
+
+        for (SootClass this_class : allClasses) {
+            String class_name = this_class.getName();
+            List<String> list_of_methods = new ArrayList<>();
+            for (SootMethod meth: this_class.getMethods()){
+                String method_name = meth.getName(); 
+                if(method_name.contains("onCreate") && class_name.contains(sootarguments[2])){
+                    list_of_methods.add(method_name+" is in the MainActivity"); 
                 }
-                else if(meth.isStatic() && cl.getName().contains(adSpecificClass)){     
-                    Print("Class:" + cl.getName());            
-                    Print("Static Method: " + meth.getName());
+                // print the class name and method name and maybe something that highlights that it is static
+                else if(meth.isStatic() && class_name.contains(advertising_specific_class)){
+                list_of_methods.add(method_name+" is static");      
                 }
-                else if(cl.hasSuperclass() && cl.getSuperclass().getName().contains("com.google.android.gms.ads.BaseAdView")){
-                    Print("Class:" + cl.getName());            
-                    Print(meth.getName());   
+                // class this method belongs to is a subclass of AdView or com.google.android.gms.ads.BaseAdView
+                else if(this_class.hasSuperclass() && this_class.getSuperclass().getName().contains(super_class_of_interest)){           
+                    list_of_methods.add(method_name+" is a subclass of AdView or com.google.android.gms.ads.BaseAdView");  
                 }
-                else if(!meth.isStatic() && cl.getName().contains(adSpecificClass)){     
-                    Print("Class:" + cl.getName());            
-                    Print(meth.getName());
+                else if(!meth.isStatic() && class_name.contains(advertising_specific_class)){                
                     // Loop through parameters and ask if subtype is of the abstract class that contains getAdUnitId
                     List<Type> parameterTypes = meth.getParameterTypes();
                     for (Type type: parameterTypes) {
-                        System.out.println("Parameter " + ": " + type);
                         // If is subtype of the abstract class call getAdUnitId
+                        if(type.toString().contains("com.google.android.gms.ads.AdView")){
+                            list_of_methods.add(method_name+" parameter is a subtype of the abstract class call getAdUnitId");
+                        }
                         // if not subtype of abstract class print name and say unsupported currently
+                        else{
+                            list_of_methods.add(method_name+" is unsupported currently");
+                        }
                     }
                 }
+                // Only add unique items to each class
+                if (list_of_methods.size() > 0){
+                    List<String> uniqueItems = new ArrayList<>();
+                    for (String item : list_of_methods) {
+                        if (!uniqueItems.contains(item)) {
+                            uniqueItems.add(item);
+                        }
+                    }
+                    dictionary_of_classes.put(class_name, uniqueItems);
+                }
+            }
+        }
+        // Iterate over classes and their methods using a dictionary_of_classes
+        for (String key : dictionary_of_classes.keySet()) {
+            Print(key + ": ");
+            List<String> items = dictionary_of_classes.get(key);
+            for (String item : items) {
+                Print("\t" + item);
             }
         }
          Main.main(new String[] {});
