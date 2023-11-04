55968f30cb907d9fc043a6e53f926ce2fc5b5757
Sean Sanders <seansanders@pop-os.localdomain>
2023-11-04 18:12:24 -0400

    Working on testing

--- Shell_Script/Run_Framework_APK_As_Output.sh
+++ Shell_Script/Run_Framework_APK_As_Output.sh
@@ -89,24 +89,6 @@ Function_Run_Framework_And_Zip_And_Sign_APK() {
 				cp $file .
 			done
 		fi
-		# adb install-multiple $(ls signed*.apk)
-		#Exit_Status=$(Function_Check_Command_Runs adb install $SignedFile)
-		# adb install $SignedFile
-	# 	device_name=$(Get_Device_Name)
-	# 	package=$(Get_App_Package $SignedFile)
-	# 	activity=$(Get_App_activity $SignedFile)
-	# 	echo Package: $package Activity: $activity
-	# 	RESULT=$?
-	# 	if [ $RESULT -eq 0 ]; then
-	# 		echo success
-	# 		#clear
-	# 		[ -d "../../../Python" ] && cd ../../../Python
-	# 		python3 Appium_Test.py $device_name $package $activity
-	# 		Uninstall_App $package
-	# 	else
-	# 		echo failed
-	# 	fi
-	# ##	adb logcat FiniteState:V *:S
 		pkill adb
 	fi
 	# rm nohup.out
@@ -138,103 +120,6 @@ Function_Test_APK_Files(){
 		cd $current_dir
 	done
 	[ -d "../../Python" ] && cd ../../Python
-	# for SignedFile in $(ls signed*.apk); do
-	# 	File_Name_Only=$(echo $SignedFile | rev | cut -d "/" -f1 | rev | sed 's/\.apk//g' | sed s/"signed"//)
-	# 	printf "\n\nFile name only is: $File_Name_Only\n"
-	# 	cp $SignedFile .APK_Testing/$File_Name_Only.apk
-	# 	printf "\nApp: $File_Name_Only\n"
-
-	# 	if [ -d APK_Testing/$File_Name_Only ]; then
-	# 		printf "\nIt exists\n"
-	# 		for file in $(ls ../../APK/APK_Testing/$File_Name_Only/signed*.apk);do
-	# 			printf "\t$file\n"
-	# 			file_name_only=$(echo $file | rev | cut -d "/" -f1 | rev | sed s/"signed"//)
-	# 			echo $file_name_only
-	# 			if ! [ $file_name_only == $File_Name_Only.apk ]; then
-	# 				echo cp2
-	# 				cp $file ../APK/APK_Testing/$File_Name_Only
-	# 			fi
-	# 		done 
-
-	# 	fi
-	# 	cd ../
-	# 	echo current_dir for Function_Test_APK_Files_1 is $(pwd)
-	# 	if [ -d ../../APK/APK_Testing/$File_Name_Only ]; then
-	# 		cd ../../APK/APK_Testing/$File_Name_Only
-	# 		echo current_dir for Function_Test_APK_Files_2 $(pwd)
-	# 		if [ -f split_config*.apk ]; then
-	# 			adb install-multiple *.apk
-	# 		else
-	# 			adb install signed*.apk
-	# 			device_name=$(Get_Device_Name)
-	# 			package=$(Get_App_Package $File_Name_Only.apk)
-	# 			activity=$(Get_App_activity $File_Name_Only.apk)
-	# 			echo Package: $package Activity: $activity
-	# 			cd ../../../Python
-	# 			RESULT=$?
-	# 			if [ $RESULT -eq 0 ]; then
-	# 				echo success
-	# 				# echo current_dir is $current_dir
-	# 				#clear
-	# 				[ -d "Python" ] && cd Python
-	# 				python3 Appium_Test.py $device_name $package $activity
-	# 				Uninstall_App $package
-	# 			else
-	# 				echo failed
-	# 			fi
-	# 		fi
-	# 	else
-	# 		printf "\n\nFile path is now F1: $(pwd)\n"
-	# 		cd APK/APK_Testing/
-	# 		printf "\n\n Curent directory is now F2: $(pwd)\n\n"
-	# 		adb install signed$File_Name_Only.apk
-	# 		device_name=$(Get_Device_Name)
-	# 		package=$(Get_App_Package $File_Name_Only.apk)
-	# 		activity=$(Get_App_activity $File_Name_Only.apk)
-	# 		echo Package: $package Activity: $activity
-	# 		cd ../../Python
-	# 			RESULT=$?
-	# 			if [ $RESULT -eq 0 ]; then
-	# 				echo success
-	# 				# echo current_dir is $current_dir
-	# 				#clear
-	# 				[ -d "Python" ] && cd Python
-	# 				python3 Appium_Test.py $device_name $package $activity
-	# 				Uninstall_App $package
-	# 			else
-	# 				echo failed
-	# 			fi
-	# 	fi
-	# done
-
-	
-	# pwd
-	# if [ -d ../../../APK/APK_Testing/$1 ]; then
-	# 	for file in $(ls  ../../../APK/APK_Testing/$1/signed*.apk | grep -v signed$1.apk)
-	# 	do
-	# 		cp $file .
-	# 	done
-	# fi
-	
-# 	adb install-multiple $(ls signed*.apk)
-# 	#Exit_Status=$(Function_Check_Command_Runs adb install $SignedFile)
-# 	# adb install $SignedFile
-# 	device_name=$(Get_Device_Name)
-# 	package=$(Get_App_Package $SignedFile)
-# 	activity=$(Get_App_activity $SignedFile)
-# 	echo Package: $package Activity: $activity
-# 	RESULT=$?
-# 	if [ $RESULT -eq 0 ]; then
-# 		echo success
-# 		#clear
-# 		[ -d "../../../Python" ] && cd ../../../Python
-# 		python3 Appium_Test.py $device_name $package $activity
-# 		Uninstall_App $package
-# 	else
-# 		echo failed
-# 	fi
-# ##	adb logcat FiniteState:V *:S
-	
 }
 Function_Run_Framework_And_Output_Jimple() {
 	echo "Running Function"
@@ -337,12 +222,6 @@ fi
 Folder=Testing
 # clear
 echo Current directory is: $(pwd)
-# for file in $(ls ../APK/$Folder/*.apk)
-# do
-# 	file_path=$(echo $file | rev | cut -d '/' -f 1 | rev | sed 's/.apk//')
-# 	echo $file_path
-# 	# echo file=$(echo $file | rev | cut -d '/' -f 1 | rev | cut -d. -f1)
-# done
 
 for file in $(ls ../APK/$Folder/*.apk)
 do
@@ -360,30 +239,9 @@ do
 	adb logcat -c
 	echo File is: $(echo $file)
 	Function_Get_MainActivity_And_Write_To_File $file $Folder
-	if [ $Option = J ] || [ $Option = j ]
-	then
-		echo "\nJimple chosen"
-		Function_Run_Framework_And_Output_Jimple $file $Option $Folder
-		# if [ $file == "TestClassAdViewAdListener_No_Listener_Call" ] 
-		if [ $file == "TestClassAdViewAdListener_No_Listener_Call" ]
-		then
-			break
-		fi
-	elif [ $Option = apk ]
-		then
-		Function_Run_Framework_And_Zip_And_Sign_APK $file $Option $Folder
-
-		# echo "Testing"
-	else
-		echo "No such option"
-	fi
+	$Option = apk
+	Function_Run_Framework_And_Zip_And_Sign_APK $file $Option $Folder
 done
-
-
-
-
-
-
 
 cd ../
 current_dir=$(pwd)
@@ -401,15 +259,10 @@ for File in $(ls signed*.apk); do
 	then
 		printf "\n\n\tFile Name only is: $file_name_only\n\n"
 		mv $File $file_name_only.apk
-		# mkdir -p $file_name_only
-		# cp $file_name_only*.apk $file_name_only/$file_name_only.apk
-		# Copy to Testing director
 		mkdir -p ../../APK/APK_Testing/$file_name_only
 		cp $file_name_only.apk ../../APK/APK_Testing/$file_name_only/$file_name_only.apk
 		if [[ -d ../../APK/Google_Play_Apps/$file_name_only && $(ls ../../APK/Google_Play_Apps/$file_name_only | wc -l) -gt 1 ]]; then
 			echo "FOUND DIR: $file_name_only!!!"
-			# mkdir -p $file_name_only
-			# cp $File $file_name_only
 			ls -1 ../../APK/Google_Play_Apps/$file_name_only/signed*.apk | grep -v '^'$file_name_only'.apk$' | grep -v '^signed'$file_name_only'.apk$' | xargs -I {} cp {} ../../APK/APK_Testing/$file_name_only
 			rm ../../APK/APK_Testing/$file_name_only/signed$file_name_only.apk
 		fi
@@ -429,12 +282,3 @@ for dir in $(ls -d */); do
 	rm $dir/*.idsig
 	rm $dir/$file
 done
-
-
-
-# cd $present_dir
-Function_Test_APK_Files
-pkill adb
- 
-# cd ../Python
-python3 Generate_Model_From_Logs.py
