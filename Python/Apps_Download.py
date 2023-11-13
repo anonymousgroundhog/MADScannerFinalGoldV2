d64caa8573a35975a31d6b6da6d0ae7a8a9b6f24
Sean Sanders <seansanders@pop-os.localdomain>
2023-11-13 15:58:20 -0500

    Adding updates to code base

--- Python/Apps_Download.py
+++ Python/Apps_Download.py
@@ -5,6 +5,13 @@ class Apps_Download:
     def __init__(self):
         self.list_of_apps_installed = [];
     
+    def create_directory_if_not_exists(self, directory_path):
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+            print(f"Directory created: {directory_path}")
+        else:
+            print(f"Directory already exists: {directory_path}")
+
     def Set_Installed_Apps(self, apps):
         self.list_of_apps_installed = apps
 
@@ -41,9 +48,6 @@ class Apps_Download:
     def Get_Apps_Phase(self):
         pwd = os.getcwd()
         app_packages = self.List_Installed_App_Packages()
-        # print(app_packages)
-        # list_of_apps_installed = ['com.android.fmradio', 'com.android.cts.priv.ctsshim', 'com.google.android.youtube', 'com.android.internal.display.cutout.emulation.corner', 'com.google.android.ext.services', 'com.android.internal.display.cutout.emulation.double', 'com.android.providers.telephony', 'com.sprd.engineermode', 'com.google.android.googlequicksearchbox', 'com.android.providers.calendar', 'com.android.providers.media', 'com.google.android.onetimeinitializer', 'com.google.android.ext.shared', 'com.android.wallpapercropper', 'com.sprd.firewall', 'com.android.documentsui', 'android.auto_generated_rro__', 'com.android.externalstorage', 'com.android.htmlviewer', 'com.android.companiondevicemanager', 'com.android.mms.service', 'com.sprd.omacp', 'com.android.providers.downloads', 'com.google.android.apps.messaging', 'com.android.sprd.telephony.server', 'com.google.android.configupdater', 'com.android.soundrecorder', 'com.android.defcontainer', 'com.android.providers.downloads.ui', 'com.android.vending', 'com.dti.blu', 'com.android.pacprocessor', 'com.android.simappdialog', 'com.sprd.validationtools', 'com.android.internal.display.cutout.emulation.tall', 'com.android.certinstaller', 'com.android.carrierconfig', 'com.google.android.marvin.talkback', 'com.google.android.apps.work.oobconfig', 'com.bluproducts.activationapp', 'android', 'com.android.camera2', 'com.android.egg', 'com.android.mtp', 'com.android.nfc', 'com.android.stk', 'com.android.launcher3', 'com.android.backupconfirm', 'com.sprd.screencapture', 'com.google.android.deskclock', 'com.sprd.quickcamera', 'com.android.statementservice', 'com.google.android.gm', 'com.unisoc.storageclearmanager', 'com.google.android.apps.tachyon', 'com.android.messaging.smilplayer', 'com.android.settings.intelligence', 'com.android.systemui.theme.dark', 'com.google.android.setupwizard', 'com.android.providers.settings', 'com.android.sharedstoragebackup', 'com.google.android.music', 'com.android.printspooler', 'com.android.dreams.basic', 'com.fiogonia.dominoes', 'com.android.se', 'com.android.inputdevices', 'com.google.android.apps.wellbeing', 'com.google.android.dialer', 'com.android.bips', 'com.google.android.apps.nbu.files', 'com.android.musicfx', 'com.google.android.apps.docs', 'com.google.android.apps.maps', 'android.autoinstalls.config.BLU.G0090', 'com.android.mmsfolderview', 'com.android.cellbroadcastreceiver', 'com.google.android.webview', 'com.opera.browser', 'com.sprd.uplmnsettings', 'com.google.android.contacts', 'com.android.server.telecom', 'com.google.android.syncadapters.contacts', 'com.android.keychain', 'com.google.android.calculator', 'com.android.chrome', 'com.sprd.uasetting', 'com.android.gallery3d', 'com.google.android.packageinstaller', 'com.spreadtrum.ims', 'com.spreadtrum.vce', 'com.google.android.gms', 'com.google.android.gsf', 'com.google.android.ims', 'com.google.android.tag', 'com.google.android.tts', 'com.ww6.agetest', 'com.android.calllogbackup', 'com.google.android.partnersetup', 'com.sprd.powersavemodelauncher', 'com.google.android.videos', 'com.android.carrierdefaultapp', 'com.android.proxyhandler', 'com.google.android.feedback', 'com.google.android.printservice.recommendation', 'com.google.android.apps.photos', 'com.sprd.logmanager', 'com.google.android.calendar', 'com.android.managedprovisioning', 'com.android.dreams.phototable', 'com.sprd.cellbroadcastreceiver', 'com.spreadtrum.vowifi', 'com.android.providers.partnerbookmarks', 'com.android.smspush', 'com.pivotmobile.android.metrics', 'com.google.android.gms.policy_sidecar_aps', 'com.securew2.paladin', 'com.sprd.providers.photos', 'com.google.android.backuptransport', 'com.android.storagemanager', 'com.android.bookmarkprovider', 'com.android.settings', 'com.spreadtrum.sgps', 'com.sprd.systemupdate', 'com.android.cts.ctsshim', 'com.sprd.sprdnote', 'com.android.vpndialogs', 'com.android.phone', 'com.android.shell', 'com.sprd.ImsConnectionManager', 'com.android.wallpaperbackup', 'com.android.providers.blockednumber', 'com.android.providers.userdictionary', 'com.android.emergency', 'com.android.location.fused', 'com.opera.preinstall', 'com.android.systemui', 'com.android.bluetoothmidiservice', 'com.android.traceur', 'com.android.modemnotifier', 'com.android.bluetooth', 'com.android.wallpaperpicker', 'com.android.providers.contacts', 'com.android.captiveportallogin', 'com.google.android.inputmethod.latin', 'com.google.android.apps.restore']
-        # list_of_apps_installed = ['com.android.fmradio', 'com.android.cts.priv.ctsshim', 'com.google.android.youtube', 'com.android.internal.display.cutout.emulation.corner', 'com.google.android.ext.services', 'com.android.internal.display.cutout.emulation.double', 'com.android.providers.telephony', 'com.sprd.engineermode', 'io.appium.settings', 'com.google.android.googlequicksearchbox', 'com.android.providers.calendar', 'com.android.providers.media', 'com.google.android.onetimeinitializer', 'com.google.android.ext.shared', 'com.android.wallpapercropper', 'com.sprd.firewall', 'com.android.documentsui', 'android.auto_generated_rro__', 'com.android.externalstorage', 'com.android.htmlviewer', 'com.android.companiondevicemanager', 'com.android.mms.service', 'com.sprd.omacp', 'com.android.providers.downloads', 'com.google.android.apps.messaging', 'com.android.sprd.telephony.server', 'com.google.android.configupdater', 'com.android.soundrecorder', 'io.appium.uiautomator2.server', 'com.android.defcontainer', 'com.android.providers.downloads.ui', 'com.android.vending', 'com.dti.blu', 'com.android.pacprocessor', 'com.android.simappdialog', 'com.sprd.validationtools', 'com.android.internal.display.cutout.emulation.tall', 'com.android.certinstaller', 'com.android.carrierconfig', 'com.google.android.marvin.talkback', 'com.sand.airdroid', 'com.google.android.apps.work.oobconfig', 'com.bluproducts.activationapp', 'android', 'com.android.camera2', 'com.android.egg', 'com.android.mtp', 'com.android.nfc', 'com.android.stk', 'com.android.launcher3', 'com.android.backupconfirm', 'com.sprd.screencapture', 'com.google.android.deskclock', 'com.sprd.quickcamera', 'com.android.statementservice', 'com.google.android.gm', 'com.unisoc.storageclearmanager', 'com.google.android.apps.tachyon', 'com.android.messaging.smilplayer', 'com.android.settings.intelligence', 'com.android.systemui.theme.dark', 'com.google.android.setupwizard', 'com.android.providers.settings', 'com.android.sharedstoragebackup', 'com.google.android.music', 'com.android.printspooler', 'com.android.dreams.basic', 'com.android.se', 'com.android.inputdevices', 'com.google.android.apps.wellbeing', 'com.google.android.dialer', 'com.android.bips', 'com.google.android.apps.nbu.files', 'com.android.musicfx', 'com.google.android.apps.docs', 'com.google.android.apps.maps', 'android.autoinstalls.config.BLU.G0090', 'com.android.mmsfolderview', 'com.android.cellbroadcastreceiver', 'com.google.android.webview', 'com.opera.browser', 'com.sprd.uplmnsettings', 'com.google.android.contacts', 'com.android.server.telecom', 'com.google.android.syncadapters.contacts', 'com.android.keychain', 'com.google.android.calculator', 'com.android.chrome', 'com.sprd.uasetting', 'com.android.gallery3d', 'com.google.android.packageinstaller', 'com.spreadtrum.ims', 'com.spreadtrum.vce', 'com.google.android.gms', 'com.google.android.gsf', 'com.google.android.ims', 'com.google.android.tag', 'com.google.android.tts', 'com.ww6.agetest', 'com.android.calllogbackup', 'com.google.android.partnersetup', 'com.sprd.powersavemodelauncher', 'com.google.android.videos', 'com.android.carrierdefaultapp', 'com.android.proxyhandler', 'com.google.android.feedback', 'com.google.android.printservice.recommendation', 'com.google.android.apps.photos', 'com.sprd.logmanager', 'com.google.android.calendar', 'com.android.managedprovisioning', 'com.android.dreams.phototable', 'com.sprd.cellbroadcastreceiver', 'io.appium.uiautomator2.server.test', 'com.spreadtrum.vowifi', 'com.android.providers.partnerbookmarks', 'com.android.smspush', 'com.pivotmobile.android.metrics', 'com.google.android.gms.policy_sidecar_aps', 'com.securew2.paladin', 'com.sprd.providers.photos', 'com.google.android.backuptransport', 'com.android.storagemanager', 'com.android.bookmarkprovider', 'com.android.settings', 'com.spreadtrum.sgps', 'com.sprd.systemupdate', 'com.android.cts.ctsshim', 'com.sprd.sprdnote', 'com.android.vpndialogs', 'com.android.phone', 'com.android.shell', 'com.sprd.ImsConnectionManager', 'com.android.wallpaperbackup', 'com.android.providers.blockednumber', 'com.android.providers.userdictionary', 'com.android.emergency', 'com.android.location.fused', 'com.opera.preinstall', 'com.android.systemui', 'com.android.bluetoothmidiservice', 'com.android.traceur', 'com.android.modemnotifier', 'com.android.bluetooth', 'com.android.wallpaperpicker', 'com.android.providers.contacts', 'com.android.captiveportallogin', 'com.google.android.inputmethod.latin', 'com.google.android.apps.restore']
         unique_packages = self.Return_App_Packages_That_Were_Installed(app_packages,self.list_of_apps_installed)
         print(unique_packages)
         for package in unique_packages:
@@ -53,14 +57,11 @@ class Apps_Download:
                 # package=package.replace(".","_")
                 time.sleep(2)
                 path_to_download_to=''.join(['../APK/Google_Play_Apps/',package,'/'])
+                self.create_directory_if_not_exists(path_to_download_to)
                 os.chdir(path_to_download_to)
-                # cmd = ''.join(['mv ' , path_to_download_to+"/base.apk ",path_to_download_to+"/",package,'.apk'])
-                # os.popen(cmd)
-                print(str(os.listdir()))
-                os.rename("base.apk", ''.join([package,'.apk']))
-                # os.mkdir(path_to_download_to)
-                # apkname=package.replace(".", "_")
-                # if os.path.isfile('base.apk'):
+
+                if os.path.exists('base.apk'):
+                    os.rename("base.apk", ''.join([package,'.apk']))
                 
                 for file in os.listdir(os.getcwd()):
                     if not file.__contains__("signed") and not file.__contains__("idsig"):
@@ -76,25 +77,5 @@ class Apps_Download:
                         shutil.copyfile(file_to_copy, destination)
 
             print("PWD TEST:"+os.getcwd())
-            # args = ('rm', '-rf', '*.idsig')
-            # subprocess.call('%s %s %s' % args, shell=True)
-        os.system("rm *.idsig")
-        os.chdir(pwd)
-        # print("PWD TEST2:" +pwd + "\npath_to_download_to:"+path_to_download_to)
+            os.chdir(pwd)
         print("COMPLETED")
-    
-
-    def Cleanup_Folders(self):
-        cwd= os.getcwd()
-        os.chdir("../APK/Google_Play_Apps")
-
-        entries = os.listdir('.')
-        directories = [entry for entry in entries if os.path.isdir(entry)]
-        os.system('clear')
-        print(os.getcwd())
-        for d in directories:
-            os.chdir(d)
-            print("\t dir is:", str(os.getcwd()))
-            print(str(os.listdir()))
-            os.popen("rm *.idsig")
-            os.chdir('../')
--- Python/Get_App_Names.py
+++ Python/Get_App_Names.py
@@ -1,53 +1,73 @@
-import requests, csv, os, pandas as pd
+import requests, csv, os, pandas as pd, hashlib
 from bs4 import BeautifulSoup
 
-def list_directories(path, directories_to_exclude):
-    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d)) and d not in directories_to_exclude]
-
-def Get_Content_Using_BeautifulSoup(url):
-	response = requests.get(url)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	headers = soup.find_all('h1')
-	app_title=""
-	app_details=[]
-	categories=[]
-	for header in headers:
-		app_title=header.get_text().strip()
-
-	elements = soup.find_all(class_='UIuSk')
-	for element in elements:
-		app_details.append(element.get_text().strip())
+class Get_App_Names:
+	def __init__(self):
+		self.base_url = 'https://play.google.com/store/apps/details?id=';
+		self.directories_list = []
 	
-	hrefs = soup.find_all('a')
-	for href in hrefs:
-		if 'category' in str(href):
-			categories.append(str(href.get("href")).split('/').pop())
-			# print(str(href.get("href")).split('/').pop())
+	def Set_Directories_To_Ignore(self, directories):
+		self.directories_list = directories
 
-	# print(soup.find('a'))
-	return url, app_title, ', '.join(app_details), ', '.join(categories)
+	def Get_File_SHA256(self, file_path):
+	    """Compute SHA256 hash of a file."""
+	    hash_sha256 = hashlib.sha256()
+	    with open(file_path, 'rb') as file:
+	        # Read and update hash in chunks of 4K
+	        for chunk in iter(lambda: file.read(4096), b""):
+	            hash_sha256.update(chunk)
+	    return hash_sha256.hexdigest()
 
-base_url='https://play.google.com/store/apps/details?id='
+	def List_Directories(self, path, directories_to_exclude):
+	    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d)) and d not in directories_to_exclude]
 
-directories_list = list_directories('../APK/Google_Play_Apps', ['BannerRecyclerViewExample', 'TestClassAdViewAdListener'])
+	def Get_Content_Using_BeautifulSoup(self, url):
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		headers = soup.find_all('h1')
+		app_title=""
+		app_details=[]
+		categories=[]
+		for header in headers:
+			app_title=header.get_text().strip()
 
-os.system('clear')
+		elements = soup.find_all(class_='UIuSk')
+		for element in elements:
+			app_details.append(element.get_text().strip())
+		
+		hrefs = soup.find_all('a')
+		for href in hrefs:
+			if 'category' in str(href):
+				categories.append(str(href.get("href")).split('/').pop())
 
+		contains_ads="No"
+		for lines in soup.find_all(class_='UIuSk'):
+			data=lines.get_text()
+			if 'Contains ads' in data:
+				print(data)
+				contains_ads="Yes"
 
-columns = ['Package Name', 'App Name', 'URL', 'Category']
-df = pd.DataFrame(columns=columns)
-for this_dir in directories_list:
-	full_url=''.join([base_url,this_dir])
-	print(full_url)
-	# print()
-	url_content = Get_Content_Using_BeautifulSoup(full_url)
-	row2 = {
-	    "Package Name": this_dir,
-	    "App Name": url_content[1],
-	    "URL": url_content[0],
-	    "Category": url_content[3],
-	}
-	df= df.append(row2, ignore_index=True)
+		package_name=url.split('=').pop()
+		sha256=self.Get_File_SHA256(''.join(['../APK/Google_Play_Apps/',package_name,'/',package_name,'.apk']))
+		return url, app_title, ', '.join(app_details), ', '.join(categories), sha256, contains_ads
 
-	df.to_csv('../Data/App_Category_Details.csv',index=False)
-print(df)
+	def Return_DataFrame_Data(self):
+		directories_list = self.List_Directories('../APK/Google_Play_Apps', self.directories_list)
+		columns = ['Package Name', 'App Name', 'URL', 'Category', 'SHA256', 'Has Ads']
+		df = pd.DataFrame(columns=columns)
+		for this_dir in directories_list:
+			full_url=''.join([self.base_url,this_dir])
+			print(full_url)
+			# print()
+			url_content = self.Get_Content_Using_BeautifulSoup(full_url)
+			row = {
+			    "Package Name": this_dir,
+			    "App Name": url_content[1],
+			    "URL": url_content[0],
+			    "Category": url_content[3],
+			    "SHA256": url_content[4],
+			    "Has Ads": url_content[5],
+			}
+			df = pd.concat([df, pd.DataFrame.from_records([row])])
+			df.to_csv('../Data/App_Category_Details.csv',index=False)
+		return df
