92136895f82cb5451ea8ed84cb9639e927c158f3
Sean Sanders <seansanders@pop-os.localdomain>
2024-01-28 15:24:51 -0500

    Adding updates to contract code

---
+++ Blockchain_Smart_Contracts/MultiAppLogTrackerV3.sol
@@ -0,0 +1,65 @@
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.0;
+    import "hardhat/console.sol";
+    contract MultiAppLogTracker {
+        string[] public log_ids;
+        string[] public things_to_check;
+        // Structure to hold log details
+        struct Log {
+            address sender;
+            string log_class;
+            string log_library;
+            string log_method;
+            string log_date_and_time;
+            uint256 timestamp;
+        }
+        struct App {
+            address sender;
+            bool valid;
+        }
+        // Mapping from app identifier to its logs
+        mapping(string => Log[]) private logs;
+        mapping(string => App[]) private apps;
+
+        // Function to add a app entry
+        function setApp(string calldata appId) external {
+            apps[appId].push(App(msg.sender, false));
+        }
+
+        // Function to add a transition entry for an app
+        function addValidTransitions(string calldata this_transition) external {
+            require(keccak256(bytes(this_transition)) != keccak256(bytes("")), "Invalid Name");
+                things_to_check.push(this_transition);
+        }
+        
+        // Function to add a log entry for an app
+        function addLog(string calldata appId, string calldata logClass, string calldata logLibrary, string calldata logMethod, string calldata logDateAndTime) external {
+            if (things_to_check.length > 0){
+                for(uint x = 0; x < things_to_check.length; x++){
+                    if(sha256(abi.encodePacked(logMethod)) == sha256(abi.encodePacked(things_to_check[x]))) {
+                        logs[appId].push(Log(msg.sender, logClass, logLibrary, logMethod, logDateAndTime, block.timestamp));
+                        log_ids.push(appId);
+                    }
+                }
+            }
+        }
+
+        // Function to retrieve logs for an app
+        // Note: This can be gas intensive for large log arrays.
+        function getLogs(string calldata appId) external view returns (Log[] memory) {
+            return logs[appId];
+        }
+
+        function getLogIds() external view returns (string[] memory) {
+            return log_ids;
+        }
+        
+        function getThingsToCheck() external view returns (string[] memory) {
+            return things_to_check;
+        }
+
+        function getAppCheck(string calldata appId) external view returns (App[] memory) {
+            return apps[appId];
+        }
+    }
+
