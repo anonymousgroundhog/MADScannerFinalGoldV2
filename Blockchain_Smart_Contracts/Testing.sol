c336a29886e412ea66203947b8473ffbb4638f4f
Sean Sanders <seansanders@pop-os.localdomain>
2024-02-07 14:37:27 -0500

    Adding update to code for adding data and exploring transitions

--- Blockchain_Smart_Contracts/Testing.sol
+++ Blockchain_Smart_Contracts/Testing.sol
@@ -1,73 +1,95 @@
-pragma solidity ^0.8.19;
-// import "github.com/Arachnid/solidity-stringutils/strings.sol";
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.0;
+    import "hardhat/console.sol";
+    // import "truffle/Console.sol";
+    contract MultiAppLogTracker {
+        string[] public log_ids;
+        string[] public things_to_check;
+        // Structure to hold log details
+        struct Log {
+            address sender;
+            string log_class;
+            string log_library;
+            string log_method;
+            string log_date_and_time;
+            uint256 timestamp;
+        }
+        struct App {
+            address sender;
+            bool valid;
+        }
+        // Mapping from app identifier to its logs
+        mapping(string => Log[]) private logs;
+        mapping(string => App[]) private apps;
 
-contract MultiAppLogTracker {
-    string[] public things_to_check;
-    string[] public log_ids;
-    struct Log {
-        address sender;
-        string methods;
-        string classes;
-        string libraries;
-        string timedate;
-    }
+        // Function to add a app entry
+        function setApp(string calldata appId) external {
+            apps[appId].push(App(msg.sender, false));
+        }
 
-    
-    // Mapping from an ID (or address) to a Log
-    mapping(string => Log[]) public logs;
-
-    // Function to append a log_method to a specific Log instance
-    function appendLogMethod(string calldata id, string memory method) public {
-        if (things_to_check.length > 0){
-            for(uint x = 0; x < things_to_check.length; x++){
-                if(sha256(abi.encodePacked(method)) == sha256(abi.encodePacked(things_to_check[x]))) {
-                    logs[id].push(Log(msg.sender, method, "classes", "libraries", "timedate"));
-                    log_ids.push(id);
+        // Function to add a transition entry for an app
+        function addValidTransitions(string calldata this_transition) external {
+            require(keccak256(bytes(this_transition)) != keccak256(bytes("")), "Invalid Name");
+                things_to_check.push(this_transition);
+        }
+        
+        // Function to add a log entry for an app
+        function addLog(string calldata appId, string calldata logClass, string calldata logLibrary, string calldata logMethod, string calldata logDateAndTime) external {
+            if (things_to_check.length > 0){
+                for(uint x = 0; x < things_to_check.length; x++){
+                    if(sha256(abi.encodePacked(logMethod)) == sha256(abi.encodePacked(things_to_check[x]))) {
+                        logs[appId].push(Log(msg.sender, logClass, logLibrary, logMethod, logDateAndTime, block.timestamp));
+                        log_ids.push(appId);
+                    }
                 }
             }
         }
-    }
 
-    // Function to create a new Log
-    function getLogs(string calldata id) external view returns (Log[] memory) {
-        return logs[id];
-    }
-    
-    function getMethodsByAppID(string calldata id) external view returns (string[] memory) {
-        string[] memory things_to_return = new string[](logs[id].length);
-            for(uint x = 0; x < logs[id].length; x++){
-                things_to_return[x] = logs[id][x].methods;
-            } 
-        return things_to_return;
-    }
-    
-    function getTuplessByAppID(string calldata id) public payable returns (string[] memory) {
+        // Function to retrieve logs for an app
+        // Note: This can be gas intensive for large log arrays.
+        function getLogs(string calldata appId) external view returns (Log[] memory) {
+            return logs[appId];
+        }
 
-        string[] memory things_to_return = new string[](logs[id].length);
-        string[] memory mytuples;
-        for(uint x = 0; x < logs[id].length; x++){
-            things_to_return[x] = logs[id][x].methods;
+        function getLogIds() external view returns (string[] memory) {
+            return log_ids;
         }
         
-        // string text = "";
-        for (uint i = 0; i < things_to_return.length; i++) {
-            if(i < things_to_return.length - 1){
-                //  text = string.concat(abi.encodePacked(things_to_return[i], ' ', things_to_return[i+1]));
-                    mytuples.push() = string.concat(things_to_return[i], things_to_return[i+1]);
-            }
-        } 
-        return mytuples;
-    }
-    function addValidTransitions(string calldata this_transition) external {
-        require(keccak256(bytes(this_transition)) != keccak256(bytes("")), "Invalid Name");
-            things_to_check.push(this_transition);
+        function getThingsToCheck() external view returns (string[] memory) {
+            return things_to_check;
+        }
+
+        function getAppCheck(string calldata appId) external view returns (App[] memory) {
+            return apps[appId];
+        }
+        // function Test(string calldata appId) external view returns (string[] memory) {
+        //     string[] memory things_to_return = new string[](apps[appId].length);
+        //     for(uint x = 0; x < logs[appId].length; x++){
+        //         things_to_return[x] = logs[appId][x].log_method;
+        //     } 
+        //     console.log(logs[appId][0].log_method);
+        //     return things_to_return;
+        // }
+        function Test(string calldata appId) view  public {
+            // string[] memory things_to_return = new string[](apps[appId].length);
+            for(uint x = 0; x < logs[appId].length; x++){
+                // things_to_return[x] = logs[appId][x].log_method;
+                if (x < logs[appId].length-1){
+                    console.log(logs[appId][x].log_method, logs[appId][x+1].log_method);
+                }
+            } 
+            
+            // return logs[appId][0].log_method;
+        }
+        // THIS IS FOR TESTING PURPOSES ONLY
+        function Populate() public {
+            this.addValidTransitions("m");
+            this.addValidTransitions("m2");
+            this.addLog("a1", "c", "lm", "m", "d");
+            this.addLog("a1", "c", "lm", "m2", "d");
+            this.addLog("a1", "c", "lm", "m", "d");
+            this.addLog("a1", "c", "lm", "m2", "d");
+        }
+        
     }
 
-    // function getValidTransitions() external view returns (string[] memory){
-    //     return things_to_check;
-    // }
-
-    function getLogIDs() external view returns (string[] memory){
-        return log_ids;
-    }
-  }
