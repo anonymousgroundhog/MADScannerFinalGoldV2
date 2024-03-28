f62f0151f6649989c208ba97607e590a08c90e84
Sean Sanders <seansanders@pop-os.localdomain>
2024-03-28 18:46:24 -0400

    Adding updates to code

--- Python/Download_Google_Play_Apps_From_Emulator.py
+++ Python/Download_Google_Play_Apps_From_Emulator.py
@@ -25,4 +25,4 @@ installed_app_packages = helper.Read_File_And_Return_Lines('Emulator_Installed_P
 download_google_play_apps_from_emulator = Download_Google_Play_Apps_From_Emulator()
 download_google_play_apps_from_emulator.Set_Apps_List(installed_app_packages)
 
-download_google_play_apps_from_emulator.Download_Apps('../APK/Google_Play_Download_Test/')
+download_google_play_apps_from_emultaor.Download_Apps('../APK/Google_Play_Download_Test/')
