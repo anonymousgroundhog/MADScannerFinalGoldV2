6ad1584624ad7cd7fb2db78f33b9b9af64f9fb66
Sean Sanders <seansanders@pop-os.localdomain>
2024-03-31 15:32:11 -0400

    Adding updates

--- Python/Analyze_APK_VirusTotalV2.py
+++ Python/Analyze_APK_VirusTotalV2.py
@@ -40,12 +40,32 @@ from pypdf import PdfReader
 
 from pyhtml2pdf import converter
 Files_To_Check_Captcha = []
-
 def Get_Data_From_VirusTotal(df):
 	pwd=os.getcwd()
 	os.chdir('../Data/VirusTotal_Screenshots')
 	url_base='https://www.virustotal.com/gui/file/'
 	for hash_value in df['App_Hashes']:
+		url = ''.join([url_base, hash_value,'/detection'])
+		file_name = ''.join([hash_value,'.pdf'])
+		cprint(hash_value, 'green')
+		if not file_name in os.listdir():
+			try:
+				converter.convert(url, file_name)
+				Files_To_Check_Captcha.append(file_name)
+			except:
+				f = open("errors.txt", "a")
+				f.write(file_name+'\n')
+				f.close()
+				cprint(''.join(["Unable to Run framework for:", hash_value]), 'red')
+				print(traceback.format_exc())
+				continue
+	os.chdir(pwd)
+
+def Get_Data_From_VirusTotal_Based_On_List(lst_hashes):
+	pwd=os.getcwd()
+	os.chdir('../Data/VirusTotal_Screenshots')
+	url_base='https://www.virustotal.com/gui/file/'
+	for hash_value in lst_hashes:
 		url = ''.join([url_base, hash_value,'/detection'])
 		file_name = ''.join([hash_value,'.pdf'])
 		cprint(hash_value, 'green')
@@ -85,6 +105,9 @@ def Remove_Files_With_Capcha():
 	os.chdir(pwd)
 
 def Analyze_PDF_Files():
+	non_malicious_files = []
+	malicious_files = []
+	undetected_files = []
 	pwd=os.getcwd()
 	os.chdir('../Data/VirusTotal_Screenshots')
 
@@ -93,21 +116,43 @@ def Analyze_PDF_Files():
 	for file in os.listdir():
 		if not file == 'errors.txt':
 			reader = PdfReader(file)
-			cprint(file, 'green')
+			cprint(file.replace('.pdf',''), 'green')
+			non_malicious_files.append(file.replace('.pdf', ''))
 			for page in reader.pages:
 				content = page.extract_text()
 				text_lower = content.lower()
 				# print(text_lower.split('\n'))
 				for line in text_lower.split('\n'):
 					# lst_line = line.split()
-					if not any(word in line for word in words):
-						print(line)
+					if not any(word in line for word in words) and len(line.split()) > 1:
+						cprint('\t'+line, 'white')
+						if 'android' in line:
+							malicious_files.append(file.replace('.pdf', ''))
+							non_malicious_files.pop()
+							cprint(line, 'red')
+						elif 'item' in line:
+							cprint(line, 'yellow')
+							non_malicious_files.pop()
+							undetected_files.append(file.replace('.pdf', ''))
+						# else:
+						# 	non_malicious_files.append(file.replace('.pdf', ''))
+
+	cprint('\n\nMalicious files are:', 'white')
+	cprint('\n'.join(malicious_files), 'red')
+
+	cprint('\n\nFiles not Detected:', 'white')
+	cprint('\n'.join(undetected_files), 'yellow')
+	
+	cprint('\n\nNormal files are:', 'white')
+	cprint('\n'.join(non_malicious_files), 'green')
 	os.chdir(pwd)
 
 os.system('clear')
 
-df = pd.read_csv('../Data/App_VirusTotal_Details.csv')
+# df = pd.read_csv('../Data/App_VirusTotal_Details.csv')
 
-Get_Data_From_VirusTotal(df)
-Remove_Files_With_Capcha()
-# Analyze_PDF_Files()
+# Get_Data_From_VirusTotal_Based_On_List(['23407e8712716302835e217065c72d8df1598f1124ad50e2d0117e1284617f8a', '9ad7266e52360765ab508e71b0dd4ba6e65541050df4783ec1f12e956c083709', '996f3e8a045b36a2a77f00367e9768279c2241e271ba3924cfd74121dc651d32', '60547eef7d4f89ada0f286c0f23bf054271821d6275b1f5840a273a20faff801', '34decd5a982c1c0e7b9a4c95cee780b4d89a457d291bc30f67210840a162e65a', '1c01d1cdb469df25e93ce5605748977c630f8538a4d1a1b5bc59af533feb4b93', 'd5e6ebb9c640a6c2c9bf64c4e6d61d046c9abe45b3b6661b4fd670385f53e4e4', 'b2dcec760ba738ab113f4bdf2d9e4f34ddfc2653ea1c4c8417248ca230acfefc', 'd314aefb43402f283efdb06b6b99dc62706e41ef9749306a56714c339774d6c5', '42643b9d8b40b50d5a37c4b6dbf846f6ed9828d0b41fc39ec5bc976035a09e96', '70c8b3e51a2432dfd8fbfaa05ab39db4457d1fe96c1e0e38389bc5ab3eb8d6e0', 'c5694e5755b3bfc96e09ee0dbd6fe0acda968c347c30f8d6e29a2e073a54c50b', '8dfd2ddbae23bd01783df6df16801c094b7e6503b0dee2651ea1d72f80f27bd6', '0aae249e7afa30356215d47dfea21841d162d48b97e7049184543f25fb603e15', '8178945c7075d84bf32c7210230d52a282ab99945b53bc53737b5235bf1cef0a', 'f399b3803986bd5c2e1a4ea160472395a72d15a16cb1af34110d7542517b6cb9', '826b0aee01a5f60eceacc29f8dcb3a776f6088b834ee0e66313138cff2d289e0', '6ce0dc7c8098e37ae9f9182f4bc9c2d44f36bcb6e4a252279fcc848a1bb3eaae', 'e176b1e1402c26f2d027f89ed880b6c8f610c9ad5136fa024e982c8ad9276baf', 'b5f7e4863d84aa4103f4332249b07f413cba4cb82565ce241d8078f04f244ac1', '0ad3d1fbca331322fe5a7b5047d5f65da6766a077cf8cbcb5328a1e880962bd8', '1b98a233d95defafe80dc3bb6f14e8cfa445a3e65cc220bb7c10ea3a7fdedcd0', '82fa87f15e8d7f0d4d1cc7d7abf4e339eade7f2321ba8d90dcd28cab20b5aa5d', 'a0390abf01aa0b310e9f5da59386596e5d91809322866974455493918dfdd05f', '15efb6f40b69f3fc9c53c94dca8198f4707e1a6366be8cf141d90060c599f756', 'fc589cb851fec04627732c7474a4c0a7c9e923f3a8a092ca9b798f987ecf17a0', 'a1331951ccb407cc2fd4ab3a4056c2eef3b36152e878838f794776f5228c754e', 'c01bf20d4185ebf0bfd638aec8ddb621b95aa684de2b09f83d3bcf73c8149f6e', '5a2b15e38cea748014d7a1edffa92a6632555f0f3c6607ae033f18118962d96c', 'e285d19234e19694df63666f17c24d1ef49df5add6c7103282bb97f1f5770126', '66ae9257e53bc72e0530b2a6afbbce0c8c66b7bbbf7b640a503e235bf06dcb6d', 'aa412475be074096e8638e06c073c55d6696ff177f8c0b0a64770c69c688d94a', '4171a34b3c6c63c8c39e62537eef7fc9113f7af3a6f65d03522eb25298cd6480', '981b5c02532f2fc3d3cec2632188d08198114f83ec2e7960f53d1880f836b0e9', 'c320e546fe41b6b4bd7edc8c611faaa5ebda21a2145968e31ae6019231f3a9fe', '7a7e98a0cfd17f27266b3256b0d975070de564c03b900e2a80d2ea293e0a320e', 'ebcce3dc547d03e472405fe521a2a58533361004e875053d03e7150c07b82d6f', '8e642d75c3d6e3a1bf2374981b347369746902c52b9617f3b854eec3cdefa784', '315a1b2bf30b96fec19ff181451958f0543c548492a320483c23ea7f91e55bcf', '7cf41d2cff0b997f71dd57ff05e41b6400a91c4fda08d3b275a4b56cf90fcb1a', 'b4173228ac21dfb59e5c080eea32ec97542e7b8fdda88d6524846ca782034cef', '266d91bbffcaf82fa04593b6434530d322a2ed0a1529943c288026953e1bc23e', 'dfdd8b1fab12bec7fa8da314ed2e2dfd6c97c2adb3e62949f7b450ee8c8ebbaf', '164605c201e064850bb777b8a49d51da99c006092182e94f184c92ad8bc687f9', '845b6194903b5a1f6d54180efb9491da1a51eea43fdfc284b68dbee87fffd29a'])
+# Remove_Files_With_Capcha()
+# Get_Data_From_VirusTotal(df)
+Analyze_PDF_Files()
+
