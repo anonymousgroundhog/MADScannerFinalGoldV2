e486b3371f806de706bcd22c1366580e4ece046e
Sean Sanders <seansanders@pop-os.localdomain>
2024-04-09 12:23:52 -0400

    Adding update

--- Python/Instrumenting_Final_Version_Testing.py
+++ Python/Instrumenting_Final_Version_Testing.py
@@ -37,6 +37,13 @@ class Instrumenting_Final_Verson_Apps:
 		self.generic_file_name = '';
 		self.source_xml = ''
 
+	def Filter_DF_Info_By_Folder_Path(self):
+		this_list = os.listdir(self.testing_folder_path)
+		this_list = [w.replace('signed', '') for w in this_list]
+		# print(this_list)
+
+		self.df_apps_filtered = self.df_apps_filtered[self.df_apps_filtered['App_Name'].isin(this_list)]
+
 	def Set_File(self, file):
 		self.file = file
 
@@ -89,7 +96,7 @@ class Instrumenting_Final_Verson_Apps:
 		desired_caps['platformName'] = 'Android'
 		# desired_caps['platformVersion'] = '33'
 		# desired_caps['automationName'] = 'uiautomator2'
-		desired_caps['deviceName'] = '7040018020065015'
+		desired_caps['deviceName'] =  'emulator-5554' #'7040018020065015'
 		desired_caps['appPackage'] = self.package_name
 		desired_caps['appActivity'] = self.main_activity
 		desired_caps['appium:wdaStartupRetries'] = '6'
@@ -260,25 +267,27 @@ class Instrumenting_Final_Verson_Apps:
 			self.generic_file_name = generic_file_name
 			df_filtered = self.df_apps_filtered['App_Name'] == generic_file_name
 			df_filtered = self.df_apps_filtered[df_filtered]
+			main_activity=df_filtered['Main_Activity']
+			main_class=df_filtered['Main_Class']
 			# print("DF class is now: ", df_filtered)
-			aapt_details=subprocess.run(['aapt', 'dump', 'badging', file], stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-			main_activity = [item for item in aapt_details if "launchable-activity" in item]
-			main_class = [item for item in aapt_details if "package" in item]
-			# cprint(''.join(['Main activity is now: ',str(main_activity)]), 'green')
-			if len(main_activity) > 0:
-				main_activity = main_activity[0].split(" ")[1].replace("name=","").replace("'","")
-			else:
-				main_activity = ''
+			# aapt_details=subprocess.run(['aapt', 'dump', 'badging', file], stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
+			# main_activity = [item for item in aapt_details if "launchable-activity" in item]
+			# main_class = [item for item in aapt_details if "package" in item]
+			# # cprint(''.join(['Main activity is now: ',str(main_activity)]), 'green')
+			# if len(main_activity) > 0:
+			# 	main_activity = main_activity[0].split(" ")[1].replace("name=","").replace("'","")
+			# else:
+			# 	main_activity = ''
 
-			if len(main_class) > 0:
-				main_class = main_class[0].replace("package: ","").split(" ")[0].replace("name=","").replace("'","")
-			else:
-				main_class = ''
+			# if len(main_class) > 0:
+			# 	main_class = main_class[0].replace("package: ","").split(" ")[0].replace("name=","").replace("'","")
+			# else:
+			# 	main_class = ''
 
-			if main_class == '':
-				main_class=''
-			if main_activity == 'NaN':
-				main_activity = ''
+			# if main_class == '':
+			# 	main_class=''
+			# if main_activity == 'NaN':
+			# 	main_activity = ''
 			self.Set_Package_Name(main_class)
 			self.Set_Main_Activity_Name(main_activity)
 			app_counter=app_counter+1
@@ -324,13 +333,14 @@ class Instrumenting_Final_Verson_Apps:
 			pwd = os.getcwd()
 			df_all_apps = pd.read_csv('../Data/App_Category_Details2.csv')
 			df_all_apps  = df_all_apps.loc[df_all_apps['Able_To_Install'] == 'Yes']
-			# lst_valid_apps= df_all_apps['']
-			print(df_all_apps)
 			os.chdir(self.testing_folder_path)
 			self.Set_Phone_Name()
 			print('Phone name is: ', self.phone_name, '\n')
 			self.df_app_info['Try_Manual_Testing'] = 'No'
-			os.system("rm *.csv")
+			try:
+				os.system("rm *.csv")
+			except:
+				cprint('No csv found!', 'yellow')
 			number_of_apps = len(df_all_apps['Able_To_Install'].values)
 			cprint("Phone name:"+self.phone_name, 'yellow')
 			app_counter=0
@@ -341,36 +351,29 @@ class Instrumenting_Final_Verson_Apps:
 				generic_file_name = file.replace('signed', '').replace(' ', '')
 				self.generic_file_name = generic_file_name
 				# cprint(self.generic_file_name, 'yellow')
-				if self.generic_file_name in df_all_apps['App_Name'].values:
+				cmd= ' '.join(['aapt dump badging',file])
+				# result = os.system(cmd)
+				direct_output = subprocess.check_output(cmd, shell=True)
+				result = str(direct_output).split('\n')
+
+				print(result)
+				# print('launchable-activity' in str(result))
+				# break
+
+				if self.generic_file_name in df_all_apps['App_Name'].values and 'launchable-activity' in str(result):
 					cmd= ' '.join(['adb install-multiple', file])
 					os.system(cmd)
 					# cprint(file, 'green')
-					df_filtered = self.df_apps_filtered['App_Name'] == generic_file_name
-					df_filtered = self.df_apps_filtered[df_filtered]
-					# print("DF class is now: ", df_filtered)
-					aapt_details=subprocess.run(['aapt', 'dump', 'badging', file], stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-					main_activity = [item for item in aapt_details if "launchable-activity" in item]
-					main_class = [item for item in aapt_details if "package" in item]
-					# cprint(''.join(['Main activity is now: ',str(main_activity)]), 'green')
-					if len(main_activity) > 0:
-						main_activity = main_activity[0].split(" ")[1].replace("name=","").replace("'","")
-					else:
-						main_activity = ''
+					df_filtered = df_all_apps[df_all_apps['App_Name'] == generic_file_name]
+					main_activity = df_filtered.Main_Activity.iloc[0]
+					main_class = df_filtered.Main_Class.iloc[0]
 
-					if len(main_class) > 0:
-						main_class = main_class[0].replace("package: ","").split(" ")[0].replace("name=","").replace("'","")
-					else:
-						main_class = ''
-
-					if main_class == '':
-						main_class=''
-					if main_activity == 'NaN':
-						main_activity = ''
 					self.Set_Package_Name(main_class)
 					self.Set_Main_Activity_Name(main_activity)
 					app_counter=app_counter+1
 					cprint(''.join(['\tRunning app: ', str(app_counter),' of ',str(number_of_apps)]), 'green')
 					print('\nApp_Name:', generic_file_name)
+					# print('\n File name for df is:',df_filtered['App_Name'])
 					print('Package name is:', self.package_name)
 					print('Main Activity name is:', self.main_activity)
 					
@@ -387,211 +390,32 @@ class Instrumenting_Final_Verson_Apps:
 
 					cmd = ' '.join(['adb uninstall', self.package_name])
 					os.system(cmd)
-			print(self.df_app_info)
+					# break
 			self.df_app_info.to_csv('testing.csv', index=False)
 			os.chdir(pwd)
 
 	def Start_Logcat(self):
-			os.system('adb logcat -c')
-			now = datetime.now()
-			d4 = now.strftime("%m-%d-%Y_%H:%M:%S")
-			cmd=''.join(['nohup adb logcat FiniteState:V *:S > ',self.logcat_path,' &'])
-			os.system(cmd)
+		cprint("Logging to: "+self.logcat_path, 'yellow')
+		os.system('adb logcat -c')
+		now = datetime.now()
+		d4 = now.strftime("%m-%d-%Y_%H:%M:%S")
+		cmd=''.join(['nohup adb logcat FiniteState:V *:S > ',self.logcat_path,' &'])
+		os.system(cmd)
 		
 	def Stop_Logcat(self):
 		os.system('pkill adb')
 
-# def Set_Capabilities(phone_name, package_name, main_activity, file):
-# 		cprint('\n\tSetting Capabilities!!!', 'cyan')
-# 		cprint(''.join(['\nSetting capabilities for dir:', os.getcwd()]), 'cyan')
-# 		cprint(os.getcwd(),'green')
-# 		desired_caps = {}
-# 		desired_caps['platformName'] = 'Android'
-# 		desired_caps['deviceName'] = '7040018020065015'
-# 		desired_caps['appPackage'] = self.package_name
-# 		desired_caps['appActivity'] = self.main_activity
-# 		desired_caps['appium:wdaStartupRetries'] = '6'
 
-# 		desired_caps['adbExecTimeout'] = '120000'
-# 		appium_server_url = 'http://localhost:4723/wd/hub'
-# 		driver = webdriver.Remote(appium_server_url, desired_caps)
-# 		# driver.implicitly_wait(20)
-# 		return driver
-
-# def click_on_button_by_class_permission(driver, id):
-# 		helper = Helper.Helper()
-# 		when_to_manual_test_params = helper.Read_File_And_Return_Lines('../../Python/Instrumentation_Helper_Files/click_on_button_by_class_permission_manual_test_params.txt')
-# 		test_params_permissions = helper.Read_File_And_Return_Lines('../../Python/Instrumentation_Helper_Files/click_on_button_allow_or_sign_in.txt')
-# 		# elements = self.driver.find_elements(By.CLASS_NAME, id)
-# 		time.sleep(2)
-# 		list_of_elements = driver.find_elements(By.CLASS_NAME, id)
-# 		for elem in list_of_elements:
-# 			# if elements == self.driver.find_elements(By.CLASS_NAME, id):
-# 			element_text = elem.text.lower()
-
-# 			list_element_text = element_text.split(" ")
-# 			print("Testing:",list_element_text)
-# 			manual_test=False
-# 			for item in list_element_text:
-# 				if item in when_to_manual_test_params:
-# 					# self.df_app_info.loc[self.df_app_info['App_Name'] == generic_file_name, 'Try_Manual_Testing'] = 'Yes'
-# 					manual_test = True
-# 					break;	
-
-# 			if manual_test:
-# 				break;
-
-# 			if element_text == 'continue with google' or 'sign up later':
-# 				try:
-# 					cprint(element_text, 'yellow')
-# 					elem.click()
-# 					break
-# 				except:
-# 					cprint(''.join(["elem doesn't exist (Unique case):",str(id)]), 'red')
-# 					continue
-			
-# 			if 'sean' in list_element_text and 'sanders' in list_element_text:
-# 				try:
-# 					cprint(element_text, 'yellow')
-# 					elem.click()
-# 					break
-# 				except:
-# 					cprint(''.join(["elem doesn't exist (Unique case):",str(id)]), 'red')
-# 					continue
-# 			# HANDLE CASE FOR ALLOWING
-# 			if element_text in test_params_permissions:
-# 				try:
-# 					cprint(element_text, 'yellow')
-# 					elem.click()
-# 					break
-# 				except:
-# 					cprint(''.join(["elem doesn't exist (Unique case):",str(id)]), 'red')
-# 					continue
-
-# 			# HANDLE SPECIAL CASE CHECKBOX
-# 			if id.__contains__('android.widget.CheckBox') and element_text.__contains__('agree') and not clicked_checkbox:
-# 				try:
-# 					cprint(element_text, 'yellow')
-# 					elem.click()
-# 					clicked_checkbox = True
-# 					break
-# 				except:
-# 					cprint(''.join(["elem doesn't exist (Unique case):",str(id)]), 'red')
-# 					continue
-
-# 			if element_text in ['agree', 'accept'] and clicked_checkbox:
-# 			    try:
-# 			        cprint(element_text, 'yellow')
-# 			        elem.click()
-# 			        break
-# 			    except:
-# 			        cprint(''.join(["elem doesn't exist:",str(id)]), 'red')
-# 			        continue
-# def click_on_button_by_class_advertisement(driver, id):
-# 		time.sleep(2)
-# 		list_of_elements = driver.find_elements(By.CLASS_NAME, id)
-# 		for elem in list_of_elements:
-# 			if source_xml == driver.page_source:
-# 				elem.click()
-# 			else:
-# 				cprint('page source xml not same. Exiting!!!', 'red')
-# 				break
-
-# def Start_Touch_Events_Only(driver, package_name, main_activity):
-# 		try:	
-# 			# if not Error_Occured:
-# 			# print("Packcage:"+str(self.package_name) + " Activity:" +str(self.main_activity))
-# 			helper = Helper.Helper()
-# 			classes_to_filter_on = helper.Read_File_And_Return_Lines('../../Python/Instrumentation_Helper_Files/click_on_button_by_class_permission.txt')
-# 			source_xml = driver.page_source
-# 			# cprint(source_xml, 'cyan')
-# 			time.sleep(2)
-# 			counter=0
-# 			while counter < 5:
-# 				for cl in classes_to_filter_on:
-# 					click_on_button_by_class_permission(driver, cl)
-# 				# source_xml = self.driver.page_source
-# 				counter=counter+1
-# 			# self.Set_Source_XML()
-# 			classes_to_filter_on_ad_specific = ['android.widget.TextView']
-# 			cprint( "Starting to click on ad specific now!!!",'red')
-# 			for cl in classes_to_filter_on_ad_specific:
-# 					click_on_button_by_class_advertisement(driver, cl)
-# 			app_activity = driver.current_activity
-# 			print("Current activity is: ",str(app_activity))
-# 			# print('\nPage source is:', source_xml)
-
-# 			click_on_screen_by_cordinates(driver, 366, 82.9, 2, 3)
-# 			click_on_screen_by_cordinates(driver, 384, 238.5, 2, 3)
-# 			this_activity = driver.current_activity
-# 			print("Activity is now:", str(this_activity))
-# 			if this_activity != app_activity:
-# 				click_on_screen_by_cordinates(driver, 582, 110, 2, 3)
-# 				click_on_screen_by_cordinates(driver, 719, 127, 2, 2)
-# 				click_on_screen_by_cordinates(driver, 363, 304, 2, 2)
-# 				click_on_screen_by_cordinates(driver, 547, 809, 2, 2)
-# 		except:
-# 			# print(colored(''.join(['Error performing actions for:', this_dir]), 'red'))
-# 			print(traceback.format_exc())
-
-# def click_on_screen_by_cordinates(driver, x, y, duration, pause_time):
-# 		time.sleep(pause_time)
-# 		try:
-# 			TouchAction(driver).tap(None, x, y, duration).perform()
-# 		except:
-# 			print(''.join(['FAILED TRYING TO TOUCH POSITION: (', str(x), ' ', str(y), ')']))
-
-# os.chdir('../APK/Valid_APK_Files_To_Test')
-
-# phone_name=subprocess.run(['adb', 'devices'], stdout=subprocess.PIPE)
-# phone_name=str(phone_name.stdout.decode('utf-8')).replace("\n","").replace("List of devices attached","").replace("device","").replace('\t','')
-# app_counter=0
-# number_of_apps = len(os.listdir())
-# for file in os.listdir():
-# 	if 'apk' in file:
-# 		print(file)
-# 		aapt_details=subprocess.run(['aapt', 'dump', 'badging', file], stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-# 		main_activity = [item for item in aapt_details if "launchable-activity" in item]
-# 		main_class = [item for item in aapt_details if "package" in item]
-# 		# cprint(''.join(['Main activity is now: ',str(main_activity)]), 'green')
-# 		if len(main_activity) > 0:
-# 			main_activity = main_activity[0].split(" ")[1].replace("name=","").replace("'","")
-# 		else:
-# 			main_activity = ''
-
-# 		if len(main_class) > 0:
-# 			main_class = main_class[0].replace("package: ","").split(" ")[0].replace("name=","").replace("'","")
-# 		else:
-# 			main_class = ''
-
-# 		if main_class == '':
-# 			main_class=''
-# 		if main_activity == 'NaN':
-# 			main_activity = ''
-# 		print('main_class:',main_class)
-# 		print('main_activity:',main_activity)
-# 		app_counter=app_counter+1
-# 		# cprint(''.join(['\tRunning app: ', str(app_counter),' of ',str(number_of_apps)]), 'green')
-# 		cmd= ' '.join(['adb install-multiple', file])
-# 		os.system(cmd)
-# 		if main_activity != '' and main_class != '':
-# 			try:
-# 				driver = Set_Capabilities(phone_name, main_class, main_activity, ''.join([os.getcwd(),'/',file]))
-# 			except:
-# 				print(''.join(['Error setting desired_capabilities for:', file]))
-# 				try:
-# 					Start_Touch_Events_Only()
-# 				except:
-# 					cprint("Error try manually testing app!!!", 'red')
-# 		os.system(''.join(['adb uninstall ', main_activity]))
 
 instrument_apps = Instrumenting_Final_Verson_Apps()
 instrument_apps.Set_DF_App_Info(pd.read_csv('../Data/App_Category_Details2.csv'))
 instrument_apps.Set_Overide_Contains_Ads(True)
 instrument_apps.Filter_DF_App_Info()
-print(instrument_apps.df_apps_filtered)
+# print(instrument_apps.df_apps_filtered)
+# instrument_apps.Filter_DF_Info_By_Folder_Path()
 instrument_apps.Set_Path_For_Logcat('../Data/Logs')
 instrument_apps.Set_Copy_From_Folder_Path('Valid_APK_Files_To_Test')
+print(instrument_apps.df_apps_filtered)
 instrument_apps.Start_Logcat()
 instrument_apps.Instrument_Apps_In_Testing_Folder_Path_Install_And_Start()
 instrument_apps.Stop_Logcat()
