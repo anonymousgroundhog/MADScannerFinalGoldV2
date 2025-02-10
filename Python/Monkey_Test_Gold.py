c60639000e13001cd7dce8c2b90670f1c200264d
anonymousgroundhog <leapfrogopus@gmail.com>
2025-02-10 15:43:08 -0600

    Working on Monkey testing
    Working on making sure the monkey test works

--- Python/Monkey_Test_Gold.py
+++ Python/Monkey_Test_Gold.py
@@ -3,21 +3,13 @@ import os, subprocess, traceback, hashlib, shutil, time, pandas as pd, glob, num
 import unittest
 from termcolor import colored, cprint
 from subprocess import run
-# Android environment
-# from appium import webdriver
-# from appium.webdriver.common.touch_action import TouchAction
-# from xml.etree import ElementTree as ET
-# from appium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import NoSuchElementException
-# from ppadb.client import Client as AdbClient
-# from appium.options.android import UiAutomator2Options
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
-
-# Import Appium UiAutomator2 driver for Android platforms (AppiumOptions)
+from selenium.webdriver.common.by import By
 from appium.options.android import UiAutomator2Options
-from path import Path
+
+from datetime import datetime
+
 def Stop_Logcat():
 	os.system('pkill adb')
 
@@ -30,7 +22,6 @@ def Set_Capabilities(phone_name, package_name, main_activity, this_file_path):
 		desired_caps['appPackage'] = package_name
 		desired_caps['appActivity'] = main_activity
 		desired_caps['appium:wdaStartupRetries'] = '6'
-
 		desired_caps['adbExecTimeout'] = '120000'
 		# desired_caps['app'] = this_file_path
 		appium_server_url = 'http://localhost:4723/wd/hub'
@@ -46,11 +37,7 @@ def Get_Phone_Name():
 	try:
 		phone_name=subprocess.run(['adb', 'devices'], stdout=subprocess.PIPE)
 		phone_name=str(phone_name.stdout.decode('utf-8')).replace("\n","").replace("List of devices attached","").replace("device","").replace('\t','')
-		# if phone_name.__contains__('offline'):
-		# 	print('emulator offline!!!', 'red')
-		# 	phone_name=''
 	except:
-		# print('No Phone Found!!!', 'red')
 		phone_name=''
 	return phone_name
 
@@ -68,10 +55,62 @@ def Get_Main_Class(this_file):
 	# cprint(cmd, 'cyan')
 	return cmd
 
+def Get_XML_Output_Of_Interest(driver, thing_to_search):
+	items_to_return = []
+	xml_source = driver.page_source
+	for element in xml_source.split('\r\n'):
+		if thing_to_search in element:
+			items_to_return.append(element.split('class=').pop().split(' ')[0].replace('"',''))
+	return items_to_return
+
+def click_on_button_by_class(driver, id):
+    for elem in driver.find_elements(By.CLASS_NAME, id):
+        if len(driver.find_elements(By.CLASS_NAME, id)) > 0 and elem.is_displayed():  
+            try:
+                print(elem)
+                elem.click()
+            except:
+                print(''.join(["elem doesn't exist:",str(id)]))
+                continue
+
+def click_on_button_by_class_close_Ad(driver, id):
+    # if (!driver.findElements(By.Id(“some_ID”)).isEmpty && driver.findElements(By.Id(“some_ID”)).get(0).isDispalyed() )
+    for elem in driver.find_elements(By.CLASS_NAME, id):
+        if 'close' in elem.text.lower() and elem.is_displayed():  
+            try:
+                print(elem)
+                elem.click()
+            except:
+                print(''.join(["elem doesn't exist:",str(id)]))
+                continue
+def click_on_button_by_class_permission(driver, id):
+        for elem in driver.find_elements(By.CLASS_NAME, id):
+            if 'allow' in elem.text.lower():
+                try:
+                    print(elem)
+                    elem.click()
+                except:
+                    print(''.join(["elem doesn't exist:",str(id)]))
+                    continue
+
+def Start_Logcat(logcat_path):
+	cprint("Logging to: "+logcat_path, 'yellow')
+	os.system('adb logcat -c')
+	now = datetime.now()
+	d4 = now.strftime("%m-%d-%Y_%H:%M:%S")
+	cmd=''.join(['nohup adb logcat FiniteState:V *:S > ',''.join([logcat_path,'/',str(d4),'.txt']),' &'])
+	os.system(cmd)
+		
+def Stop_Logcat():
+	os.system('pkill adb')
+
+
 os.system('clear')
 # os.chdir('../')
 cwd=os.getcwd()
+Start_Logcat('../Data/Logs')
 os.chdir('../Java/Classes')
+
 for file in os.listdir():
 	if not os.path.isfile(file):
 		os.chdir(file)
@@ -90,16 +129,25 @@ for file in os.listdir():
 				cprint(' '.join(['Main activity:', main_activity]), 'yellow')
 				cprint(' '.join(['Main class:', main_class]), 'yellow')
 				cprint('RUNNING Monkey TEST', 'green')
-				# try:
-				cmd= ' '.join(['adb install-multiple', signed_file])
-				os.system(cmd)
-				print('\n\tSetting Capabilities!!!', 'cyan')
-				# cprint(''.join(['\nSetting capabilities for dir:', os.getcwd()]), 'cyan')
-				cprint(os.getcwd(),'green')
-				Set_Capabilities(phone_name,main_class,main_activity,signed_file)
-				time.sleep(5)
-				# except:
-				# 	cprint('Unable to install'+signed_file, 'yellow')
+				try:
+					cmd= ' '.join(['adb install-multiple', signed_file])
+					os.system(cmd)
+					cprint('\n\tSetting Capabilities!!!', 'cyan')
+					# cprint(''.join(['\nSetting capabilities for dir:', os.getcwd()]), 'cyan')
+					cprint(os.getcwd(),'green')
+					driver = Set_Capabilities(phone_name,main_class,main_activity,signed_file)
+					xml_elements = Get_XML_Output_Of_Interest(driver, 'android.widget.TextView')
+
+					for item in xml_elements:
+						time.sleep(2)
+						try:
+							click_on_button_by_class(driver, item)
+						except:
+							cprint('unable to click anymore', 'yellow')
+					cprint(xml_elements, 'green')
+					time.sleep(5)
+				except:
+					cprint('Unable to install ' + signed_file, 'yellow')
 				cmd = ' '.join(['adb uninstall', main_class])
 				os.system(cmd)
 
@@ -108,3 +156,6 @@ for file in os.listdir():
 				cprint('Make sure appium server is running, emulator android device is running, and retry again!!!', 'yellow')
 
 		os.chdir("../")
+
+os.chdir(cwd)
+Stop_Logcat()		
