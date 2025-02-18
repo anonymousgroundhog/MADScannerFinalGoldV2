5985810cf1741282f67f4b4429c1ed476c423f90
anonymousgroundhog <leapfrogopus@gmail.com>
2025-02-18 16:44:12 -0600

    Adding updates to scraping code

--- Python/Scrape_Logs_Gold.py
+++ Python/Scrape_Logs_Gold.py
@@ -1,4 +1,4 @@
-import os
+import os, pandas as pd
 import Blockchain_Gold as Blockchain
 from termcolor import colored, cprint
 
@@ -13,10 +13,10 @@ blockchain.Set_Sender_Address('0x07842b8C2EC9D353b7aBe41F77AaF01AB79c73Dd')
 blockchain.Set_ABI('[ { "inputs": [ { "internalType": "string", "name": "appId", "type": "string" }, { "internalType": "string", "name": "logClass", "type": "string" }, { "internalType": "string", "name": "logLibrary", "type": "string" }, { "internalType": "string", "name": "logMethod", "type": "string" }, { "internalType": "string", "name": "logDateAndTime", "type": "string" } ], "name": "addLog", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "elementToLookFor", "type": "string" } ], "name": "exists_in_logs_array", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getLogIds", "outputs": [ { "internalType": "string[]", "name": "", "type": "string[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "log_ids", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "appID", "type": "string" } ], "name": "returnLogs", "outputs": [ { "components": [ { "internalType": "string[]", "name": "log_class", "type": "string[]" }, { "internalType": "string[]", "name": "log_library", "type": "string[]" }, { "internalType": "string[]", "name": "log_method", "type": "string[]" }, { "internalType": "string[]", "name": "log_date_and_time", "type": "string[]" } ], "internalType": "struct MultiAppLogTracker.Log", "name": "", "type": "tuple" } ], "stateMutability": "view", "type": "function" } ]')
 blockchain.Set_Contract_Address('0x11242d4E5A9E64e07351A3f8Eb65E6335B93A807')
 blockchain.Set_Contract_Instance()
-
+df = pd.DataFrame(columns=['dates', 'hashes', 'classes', 'methods'])
 for file in os.listdir():
     if 'txt' in file:
-        print(file)
+        # print(file)
         file_name= file.replace(' ', '').replace('.txt','')
 
         with open(file) as file:
@@ -31,9 +31,28 @@ for file in os.listdir():
                 content_to_manipulate=content_to_manipulate.replace(' ','')
                 content_to_manipulate = content_to_manipulate.split('---')
                 if len(content_to_manipulate) >= 4:
-                    this_date=content_to_manipulate [0]
+                    this_date=content_to_manipulate [0].split('_')[0]
                     this_hash=content_to_manipulate [2]
                     this_class=content_to_manipulate [3]
                     this_method=content_to_manipulate [4]
-                    blockchain.Add_Data(this_hash,this_hash, this_class, this_method, this_date)
 
+                    new_row={'dates': this_date, 'hashes':this_hash, 'classes':this_class, 'methods':this_method}
+                    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+
+df['dates'] = df['dates'].fillna(pd.to_datetime('1900-01-01'))
+df['dates'] = pd.to_datetime(df['dates'])
+most_recent_date = df['dates'].max()
+most_recent_year = most_recent_date.year
+most_recent_month = most_recent_date.month
+most_recent_day = most_recent_date.day
+most_recent_logs = df[
+    (df['dates'].dt.year == most_recent_year) &
+    (df['dates'].dt.month == most_recent_month) &
+    (df['dates'].dt.day == most_recent_day)
+]
+
+most_recent_logs.reset_index(inplace=True)
+print(most_recent_logs)
+
+# blockchain.Add_Data(this_hash,this_hash, this_class, this_method, this_date)
